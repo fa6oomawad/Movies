c024a6b2d62cdc34c866a86105232f36a094db64
fatima awad <fatimamuk0@gmail.com>
2019-02-10 20:28:21 -0500

    changed the state handling to diff component

--- src/components/App.js
+++ src/components/App.js
@@ -10,39 +10,6 @@ import {
 } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      data:[]
-    }
-this.fetchMoviesData=this.fetchMoviesData.bind(this);
-  }
-  componentDidMount(){
-this.fetchMoviesData();
-  }
-
-  fetchMoviesData(){
-const encodedURI=encodeURI('https://api.themoviedb.org/3/discover/movie?api_key=9f9325b5853f1d7c7965f643d2cf0d51&primary_release_date.gte=2019-01-11&primary_release_date.lte=2019-2-10&total_results=8');
-return fetch(encodedURI)
-.then((data)=>data.json())
-.then ((repos)=>{
-  console.log(repos.results)
-  this.setState({
-data:repos.results
-  }
-  )
-
-  console.log(this.state.data)
-
-})
-.catch((error)=>{
-  console.warn(error)
-  return null 
-});
-     }
-
-
-
   render() {
 
     return (
@@ -56,8 +23,7 @@ data:repos.results
 <hr />
 
 <Route exact path='/' component={Home}/>
-<Route path='/Movies' 
-render={(props)=> <Movies {...props} data={this.state.data}/>}/>
+<Route path='/Movies' component ={Movies}/>
 <Route path='/TvShowes' component={TvShowes}/>
 
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './movies.css';
 import OneMovie from './OneMovie.js';
 import {
    Link, 
     Route
 } from 'react-router-dom';
- export default function Movies (props){
-     return (<div>
+
+ class  Movies extends Component{
+     constructor(props){
+         super(props);
+         this.state={
+             data:[]
+         }
+         this.fetchMoviesData=this.fetchMoviesData.bind(this);
+
+     }
+
+
+     componentDidMount(){
+        this.fetchMoviesData();
+          }
+        
+          fetchMoviesData(){
+        const encodedURI=encodeURI('https://api.themoviedb.org/3/discover/movie?api_key=9f9325b5853f1d7c7965f643d2cf0d51&primary_release_date.gte=2019-01-11&primary_release_date.lte=2019-2-10&total_results=8');
+        return fetch(encodedURI)
+        .then((data)=>data.json())
+        .then ((repos)=>{
+          console.log(repos.results)
+          this.setState({
+        data:repos.results
+          }
+          )
+        
+          console.log(this.state.data)
+        
+        })
+        .catch((error)=>{
+          console.warn(error)
+          return null 
+        });
+             }
+
+     render(){
+     return (
+     <div>
 <div className='box'>
-    
-    
-    {console.log(props.data)}
-    {props.data.slice(0,8).map((ele)=>(
+    {console.log(this.state.data)}
+    {this.state.data.slice(0,8).map((ele)=>(
         <div className='smallbox'>
         <Link to="Movies/movie1">
         <img src={`http://image.tmdb.org/t/p/w185${ele.poster_path}`} alt="movie pic"/>
         </Link>
         </div>
     ))}
-    <Route  path={`Movies/:movie1`} component={OneMovie}/>
+    
+    <Route path='/movie1' 
+render={(props)=> <OneMovie {...props} data={this.state.data}/>}/>
 </div>
 </div>
      
      )
- }
+ }}
+
+ export default Movies;
