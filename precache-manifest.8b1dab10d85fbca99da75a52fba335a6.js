c33e6489935524af965ca33b91ce744948213d11
fatima awad <fatimamuk0@gmail.com>
2019-02-25 19:34:07 -0500

    Updates

---
+++ precache-manifest.8b1dab10d85fbca99da75a52fba335a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ad4d8afb5a54c964d9f",
+    "url": "/movies/static/js/runtime~main.6ad4d8af.js"
+  },
+  {
+    "revision": "08a5747c4b79ed7fee50",
+    "url": "/movies/static/js/main.08a5747c.chunk.js"
+  },
+  {
+    "revision": "76c311a0f7bd61d7ecf5",
+    "url": "/movies/static/js/1.76c311a0.chunk.js"
+  },
+  {
+    "revision": "08a5747c4b79ed7fee50",
+    "url": "/movies/static/css/main.62efd259.chunk.css"
+  },
+  {
+    "revision": "900131787d7347a6fe83550815a429d4",
+    "url": "/movies/index.html"
+  }
+];
